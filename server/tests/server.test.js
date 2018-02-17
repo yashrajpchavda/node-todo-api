7858d93cc9163da8a38b541822f9d98633ef3cda
Yashraj Chavda <EZDI\yashraj.c@yashrajc.ezdi.local>
2018-02-17 15:11:41 +0530

    add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require( "expect" );
 const request = require( "supertest" );
+const { ObjectID } = require( "mongodb" );
 
 const { app } = require( "./../server" );
 const { Todo } = require( "./../models/todo" );
 
 const todos = [ {
+  _id : new ObjectID(),
   text: "First test todo"
 }, {
+  _id : new ObjectID(),
   text: "Second test todo"
 } ];
 
@@ -67,8 +70,44 @@ describe( "GET /todos", () => {
       .get( "/todos" )
       .expect( 200 )
       .expect( ( res ) => {
-        expect( res.body.todos.length ).toBe(2);
+        expect( res.body.todos.length ).toBe( 2 );
       } )
-      .end(done);
+      .end( done );
   } );
+} );
+
+describe( "GET /todos/:id", () => {
+
+  it( "should return the matching todo for the specified id", ( done ) => {
+
+    request( app )
+      .get( `/todos/${ todos[ 0 ]._id.toHexString()}` )
+      .expect( 200 )
+      .expect( ( res ) => {
+        expect( res.body.todo.text ).toBe( todos[ 0 ].text );
+      } )
+      .end( done );
+
+  } );
+
+  it( "should return 404 if todo not found", ( done ) => {
+
+    const newId = new ObjectID().toHexString();
+
+    request( app )
+      .get( `/todos/${newId}` )
+      .expect( 404 )
+      .end( done );
+
+  } );
+
+  it( "should return 404 if the invalid id was specified", ( done ) => {
+    const invalidId = "234242adsfa";
+
+    request( app )
+      .get( `/todos/${invalidId}` )
+      .expect( 404 )
+      .end( done );
+  } );
+
 } );
