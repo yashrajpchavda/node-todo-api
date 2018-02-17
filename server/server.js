199921be09d1e05e38d8a9cc504775a856d8c393
Yashraj Chavda <EZDI\yashraj.c@yashrajc.ezdi.local>
2018-02-17 20:41:32 +0530

    add configuration for heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
-const { ObjectID } = require( 'mongodb' );
+const { ObjectID } = require( "mongodb" );
 
 const { mongoose } = require( "./db/mongoose" );
 const { Todo } = require( "./models/todo" );
 const { User } = require( "./models/user" );
 
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.use( bodyParser.json() );
 
@@ -60,8 +62,8 @@ app.get( "/todos/:id", ( req, res ) => {
 
 } );
 
-app.listen( 3000, () => {
-  console.log( "started on port 3000" );
+app.listen( port, () => {
+  console.log( `started on port ${port}` );
 } );
 
 module.exports = { app };
