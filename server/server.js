a59c5f59be31961d54e392235672cf6544e76202
Yashraj Chavda <EZDI\yashraj.c@yashrajc.ezdi.local>
2018-02-17 14:55:37 +0530

    add route for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
+const { ObjectID } = require( 'mongodb' );
 
 const { mongoose } = require( "./db/mongoose" );
 const { Todo } = require( "./models/todo" );
@@ -34,6 +35,28 @@ app.get( "/todos", ( req, res ) => {
     .catch( ( err ) => {
       res.status( 400 ).send( err );
     } );
+
+} );
+
+app.get( "/todos/:id", ( req, res ) => {
+
+  const todoId = req.params.id;
+
+  // validate the id
+  if ( !ObjectID.isValid( todoId ) ) {
+    return res.status( 404 ).send();
+  }
+
+  Todo.findById( todoId )
+    .then( ( todo ) => {
+
+      if ( !todo ) {
+        return res.status( 404 ).end();
+      }
+
+      return res.send( { todo } );
+    } )
+    .catch( ( e ) => res.status( 400 ).send( e ) );
 
 } );
 
