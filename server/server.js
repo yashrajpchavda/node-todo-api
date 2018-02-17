9388603cf2a413ca2694a1f21c89cd79a2911f1f
Yashraj Chavda <EZDI\yashraj.c@yashrajc.ezdi.local>
2018-02-17 23:11:06 +0530

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require( "lodash" );
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
 const { ObjectID } = require( "mongodb" );
@@ -79,6 +80,35 @@ app.delete( "/todos/:id", ( req, res ) => {
     } )
     .catch( ( err ) => {
       return res.status( 400 ).send();
+    } );
+
+} );
+
+app.patch( "/todos/:id", ( req, res ) => {
+
+  const todoId = req.params.id;
+  const body = _.pick( req.body, [ "text", "completed" ] );
+
+  if ( !ObjectID.isValid( todoId ) ) {
+    return res.status( 404 ).send();
+  }
+
+  if ( _.isBoolean( body.completed ) && body.completed ) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate( todoId, { $set: body }, { new: true } )
+    .then( ( todo ) => {
+      if ( !todo ) {
+        return res.status( 404 ).send();
+      }
+      res.send( { todo } );
+    } )
+    .catch( ( err ) => {
+      res.status( 400 ).send( err );
     } );
 
 } );
