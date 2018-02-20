38d4d834fe4c2035a2b817e40e19161a0a7c71a8
Yashraj Chavda <EZDI\yashraj.c@yashrajc.ezdi.local>
2018-02-20 21:51:31 +0530

    update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -36,7 +36,7 @@ const UserSchema = new mongoose.Schema( {
 UserSchema.methods.generateAuthToken = function () {
   const user = this;
   const access = "auth";
-  const token = jwt.sign( { _id: user._id.toHexString(), access }, "abc123" ).toString();
+  const token = jwt.sign( { _id: user._id.toHexString(), access }, process.env.JWT_SECRET ).toString();
 
   user.tokens = user.tokens.concat( { access, token } );
 
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function ( token ) {
   let decoded;
 
   try {
-    decoded = jwt.verify( token, "abc123" );
+    decoded = jwt.verify( token, process.env.JWT_SECRET );
   } catch ( e ) {
     return Promise.reject();
   }
